f9b69b748d0bb025323434e6baf67c7d90d81d9a
Nisha <nisha@zipcodes-mbp-8.myfiosgateway.com>
2021-06-26 17:19:37 -0400

    I changed code

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,70 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
-    }
+        
+        String s = "";
+    
+       for (int i=0; i < stop; i++){
+            s = s + String.valueOf(i);
+        }
+        return s;
+          }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String s= "";
+    
+       for (int i=start; i < stop; i++){
+            s = s + String.valueOf(i);
+        }
+        return s;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String s= "";
+    
+       for (int i=start; i < stop; i= i+step){
+            s = s + String.valueOf(i);
+        }
+        return s;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String s= "";
+    
+       for (int i=start; i < stop; i++){
+           if ((i%2) == 0){
+           s = s + String.valueOf(i);
+        }  
+       }
+        return s;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String s= "";
+    
+       for (int i=start; i < stop; i++){
+           if ((i%2) != 0){
+            s = s + String.valueOf(i);
+        }
+        }
+        return s;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String s= "";
+    
+       for (int i=start; i <= stop; i++){
+           int x = i;
+           for(int j=2; j<=exponent; j++){
+            
+               x = x * i;
+               }
+           s = s + String.valueOf(x);
+
+       }
+        return s;
     }
 }
